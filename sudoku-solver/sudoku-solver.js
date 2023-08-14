5c89fe77011379f36676676fe3b1e77bffe811fc
Ambatobe Malango <ambatobemalango@gmail.com>
2023-08-13 23:59:31 -0500

    trying to solve how the puzzle will be auto generated

--- sudoku-solver/sudoku-solver.js
+++ sudoku-solver/sudoku-solver.js
@@ -2,6 +2,7 @@ window.onload= e => {
     var containerGrid= document.getElementById("grid2");
     var biggestNum= null;
     var sumbmitMax= document.getElementById("submit-max-number");
+    var createdGrids= false;
     
     // we need for loop so we can make the little boxes in the sudoku puzzle
     function createGrids(){ // maybe in the future the grid will be self aware/like daedelus labyrinth
@@ -16,15 +17,22 @@ window.onload= e => {
                 subGridBoxNode.max=biggestNum;
                 subGridBoxNode.min=1;
                 subGridNode.appendChild(subGridBoxNode);
+                subGridBoxNode.style.cssTex="grid-template-columns: repeat(" + Math.sqrt(biggestNum) + "," + " 1fr) !important;";
+
                 //these are the individual boxes that will hold the numbers
             }
             containerGrid.appendChild(subGridNode);
+            containerGrid.style.cssTex="grid-template-columns: repeat(" + Math.sqrt(biggestNum) + "," + " 1fr) !important;";
 
         }
     }
     sumbmitMax.addEventListener("click",e =>{
-        biggestNum=parseInt(document.getElementById("max").innerText);;
-        createGrids();
-    })
+        biggestNum=parseInt(document.getElementById("max").value);;
+        if (!createdGrids && biggestNum && biggestNum%1==0){
+            createGrids(); // now that the user has submitted the number we can make the grid and subgrids
+            createdGrids=true;
+        }
+        console.log(biggestNum)
+    });
 
 }
