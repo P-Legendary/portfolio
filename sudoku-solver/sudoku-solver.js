77ca96852b811c7c26ebd37d323edbe7f16f5d44
Ambatobe Malango <ambatobemalango@gmail.com>
2023-08-21 22:21:24 -0500

    minor changes

--- sudoku-solver/sudoku-solver.js
+++ sudoku-solver/sudoku-solver.js
@@ -7,10 +7,12 @@ window.onload= e => {
     
     // we need for loop so we can make the little boxes in the sudoku puzzle
     function createGrids(){ // maybe in the future the grid will be self aware/like daedelus labyrinth
+        
         for (let i=1; i<=biggestNum; i++){
             let subGridNode= document.createElement("div");
             subGridNode.className="sub-grid";
             subGridNode.id=i;
+            
             for (let j=1; j<=biggestNum; j++){
                 let subGridBoxNode= document.createElement("input");
                 subGridBoxNode.className="sub-grid-box";
@@ -18,19 +20,21 @@ window.onload= e => {
                 subGridBoxNode.max=biggestNum;
                 subGridBoxNode.min=1;
                 subGridNode.appendChild(subGridBoxNode);
-                subGridNode.style.cssText="border: 5px black solid;";
                 subGridNode.style.cssText="grid-template-columns: repeat(" + Math.sqrt(biggestNum) + "," + " 1fr) !important;";
 
                 //these are the individual boxes that will hold the numbers
             }
+
             containerGrid.appendChild(subGridNode);
             containerGrid.style.cssText="grid-template-columns: repeat(" + Math.sqrt(biggestNum) + "," + " 1fr) !important;";
 
         }
     }
     function isPerfectSquare(number){
+
         if (Math.sqrt(number)%1==0)
             return true;
+
         return false;
     } 
     // down here is where we will make the logic for solving a puzzle that is entered if it's even solvable..
